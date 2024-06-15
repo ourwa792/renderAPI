d8d6832344430fb59039d7b5a7810c63895a8ca5
ourwa <ourwa94svu@gmail.com>
2024-06-16 00:28:49 +0200

    render API

---
+++ config/config.js
@@ -0,0 +1,20 @@
+/* const dotenv = require('dotenv')
+dotenv.config() */
+
+module.exports =
+{
+  "development": {
+    "username": "sql12714097" ,
+    "password": "EgTPLDFI2L" ,
+    "database": "sql12714097" ,
+    "host": "sql12.freesqldatabase.com",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "sql12714097",
+    "password": "EgTPLDFI2L",
+    "database": "sql12714097",
+    "host": "sql12.freesqldatabase.com",
+    "dialect": "mysql"
+  }
+}
